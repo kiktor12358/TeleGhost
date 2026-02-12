295ade1e1bd1d15c070ad1764717371faef3dd8f
TeleGhost Dev <dev@teleghost.local>
2026-02-12 09:51:20 +0500

    Fix Android CI: Split CGO flags by arch and add library copying steps

---
+++ internal/network/i2pd/cgo_android_amd64.go
@@ -0,0 +1,6 @@
+package i2pd
+
+/*
+#cgo android,amd64 LDFLAGS: -L${SRCDIR}/lib/x86_64 -li2pdclient -li2pd -li2pdlang -lssl -lcrypto -lz -latomic -lm -ldl -llog
+*/
+import "C"
---
+++ internal/network/i2pd/cgo_android_arm.go
@@ -0,0 +1,6 @@
+package i2pd
+
+/*
+#cgo android,arm LDFLAGS: -L${SRCDIR}/lib/armeabi-v7a -li2pdclient -li2pd -li2pdlang -lssl -lcrypto -lz -latomic -lm -ldl -llog
+*/
+import "C"
